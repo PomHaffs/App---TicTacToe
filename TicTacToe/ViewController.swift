c7b040572276ad9e4eb0756b7e7db7166fe02283
PomHaffs <tomashaffenden@icloud.com>
2016-12-17 08:10:10 +1100

    added to game logic

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 1
+    
+    var playerOneScore:[Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var playerTwoScore:[Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        
+        if counter % 2 == 0 {
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
